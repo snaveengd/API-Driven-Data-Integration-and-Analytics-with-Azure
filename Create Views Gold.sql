bdfbde662cf3b99552c4595bf61d6203795009cc
Naveen <navee.shiga@gmail.com>
2024-11-20 23:07:26 +0530

    Update Create Views Gold.sql

--- Create Views Gold.sql
+++ Create Views Gold.sql
@@ -11,7 +11,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Calendars/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Calendars/',
             FORMAT = 'PARQUET'
         ) AS QUER1;
 
@@ -28,7 +28,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Customerss/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Customerss/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
@@ -44,7 +44,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Products/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Products/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
@@ -59,7 +59,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Returns/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Returns/',
             FORMAT = 'PARQUET'
         ) as QUER1
         
@@ -74,7 +74,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Product_Categories/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Product_Categories/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
@@ -89,7 +89,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Sales/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Sales/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
@@ -104,7 +104,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/Product_Subcategories/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/Product_Subcategories/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
@@ -120,7 +120,7 @@ SELECT
 FROM 
     OPENROWSET
         (
-            BULK 'https://anishlambastoragedatalak.blob.core.windows.net/silver/AdventureWorks_Territories/',
+            BULK 'https://storagedatalak.blob.core.windows.net/silver/AdventureWorks_Territories/',
             FORMAT = 'PARQUET'
         ) as QUER1
 
